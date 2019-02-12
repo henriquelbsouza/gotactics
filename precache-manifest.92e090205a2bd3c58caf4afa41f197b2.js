d6d699bdccef5b15bfbf61bd15068d739d8fbf7a
Thiago Felix <thiago.felix@alphasights.com>
2019-02-12 12:48:38 -0200

    Updates

---
+++ precache-manifest.92e090205a2bd3c58caf4afa41f197b2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a3b23deda454abac3642f77ee63e006",
+    "url": "/gotactics/static/media/de_inferno_radar.8a3b23de.png"
+  },
+  {
+    "revision": "6d02a69ccc936c07c0d2",
+    "url": "/gotactics/static/css/main.188cd50d.chunk.css"
+  },
+  {
+    "revision": "20b1906bb6c2bda75152",
+    "url": "/gotactics/static/js/runtime~main.20b1906b.js"
+  },
+  {
+    "revision": "eafa51362c75c3c878e9",
+    "url": "/gotactics/static/js/2.eafa5136.chunk.js"
+  },
+  {
+    "revision": "bdad0cbd635bdeaf357c046b2137c666",
+    "url": "/gotactics/static/media/de_cache_radar.bdad0cbd.png"
+  },
+  {
+    "revision": "77e473759e9aa6b0a19988b487845e49",
+    "url": "/gotactics/static/media/de_cbble_radar.77e47375.png"
+  },
+  {
+    "revision": "6d02a69ccc936c07c0d2",
+    "url": "/gotactics/static/js/main.6d02a69c.chunk.js"
+  },
+  {
+    "revision": "222889980f38d5c4b687d395b586fef0",
+    "url": "/gotactics/static/media/de_mirage_radar.22288998.png"
+  },
+  {
+    "revision": "93291cb6eaaa6678ca42c00064abc3a8",
+    "url": "/gotactics/static/media/de_overpass_radar.93291cb6.png"
+  },
+  {
+    "revision": "3ab115c6364d63d5ca94eb2d060daece",
+    "url": "/gotactics/static/media/de_train_radar.3ab115c6.png"
+  },
+  {
+    "revision": "e15c848ff517ef069cce412678e3362a",
+    "url": "/gotactics/static/media/de_dust2_radar.e15c848f.png"
+  },
+  {
+    "revision": "16b7555c8140548172b8b902249b6b9c",
+    "url": "/gotactics/static/media/de_nuke_both.16b7555c.png"
+  },
+  {
+    "revision": "7831fec13d2bd0dc095a6b5bd5fcbeea",
+    "url": "/gotactics/index.html"
+  }
+];
